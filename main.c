224dbadad5b77bc445f3d38bb61744f1a4efa4d3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-16 21:41:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,9 @@ int main(int argc, char **argv)
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
+
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
