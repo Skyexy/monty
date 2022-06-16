0ee5304b31d35c91bbd16d9afaf5c8636e92db89
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-16 21:03:40 +0100

    Create other_function.c

---
+++ other_function.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+instruction_t instructions[] = 
+{
+	{"push", push},
+	{NULL, NULL},
+}
+/**
+ * opcode - function in charge of running builtins
+ * @stack: stack given by main
+ * @str: string to compare
+ * @line_cnt: ammount of lines
+ *
+ * Return: nothing
+ */
+void opcode(stack_t **stack, char *str, unsigned int line_cnt)
+{
+	int i = 0;
+	printf("%s", str);
+	return;
+
+	while (instructions[i].opcode)
+	{
+		if (strcmp(instructions[i].opcode, str) == 0)
+		{
+			op[i].f(stack, line_cnt);
+			return; /* if we found a match, run the function */
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
+	status = EXIT_FAILURE;
+}
