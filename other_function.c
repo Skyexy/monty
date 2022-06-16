e7f895cfb13f702a2359f685211b39c752c367b5
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-16 21:29:15 +0100

    Update other_function.c

--- other_function.c
+++ other_function.c
@@ -3,9 +3,7 @@
 #include <string.h>
 #include "monty.h"
 
-instruction_t instructions = {
-	{NULL , NULL},
-}
+
 /**
  * opcode - function in charge of running builtins
  * @stack: stack given by main
@@ -18,17 +16,6 @@ void opcode(stack_t **stack, char *str, unsigned int line_cnt)
 {
 	int i = 0;
 	printf("%s", str);
-	return;
-
-	while (instructions[i].opcode)
-	{
-		if (strcmp(instructions[i].opcode, str) == 0)
-		{
-			instructions[i].f(stack, line_cnt);
-			return; /* if we found a match, run the function */
-		}
-		i++;
-	}
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
 	status = EXIT_FAILURE;
+	return;
 }
