7fc582bff9243777694891f14b388bcaf798d384
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-16 21:07:42 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,3 +1,6 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -26,3 +29,6 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+opcode(stack_t **stack, char *str, unsigned int line_cnt);
+
+#endif /* MONTY */
