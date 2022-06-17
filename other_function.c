6cf3ede6a1ecc8f57ec2bafc50ef2344a4bb6897
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-17 01:32:52 +0100

    Update other_function.c

--- other_function.c
+++ other_function.c
@@ -6,6 +6,7 @@
 
 instruction_t instructions[] = {
 	{"push", push},
+	{"pall", pall},
 	{NULL, NULL},
 };
 
@@ -70,6 +71,37 @@ void push(stack_t **stack, unsigned int line_cnt)
 		(*stack)->prev = new;
 	*stack = new;
 	return;
+}
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main
+ * @line_cnt: ammount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	print_stack(*stack);
+}
+
+/**
+ * print_stack - prints the contents of a stack_t stack
+ * @stack: stack head
+ *
+ * Return: number of elements of the list
+ */
+size_t print_stack(const stack_t *stack)
+{
+	size_t c = 0;
+	
+	while (stack)
+	{
+		printf("%d\n", (stack)->n);
+		stack = stack->next;
+		c++;
+	}
+	return(c);
 }
 
 /**
