eeb67b0f81f6a92c8aba273dd2a2827b4abd2af5
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-17 19:18:39 +0100

    Update functions_2.c

--- functions_2.c
+++ functions_2.c
@@ -19,7 +19,7 @@ void sub(stack_t **stack, unsigned int line_cnt)
 	pop(stack, line_cnt);/*For top node*/
 	(*stack)->n = result;
 }
-void div(stack_t **stack, unsigned int line_cnt)
+void _div(stack_t **stack, unsigned int line_cnt)
 {
 	int result;
 
