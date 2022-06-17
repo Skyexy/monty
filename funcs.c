03309a513ce4e3cee82557f7f51b55db41a8ab19
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-17 23:13:22 +0100

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -31,23 +31,22 @@ void pstr(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
 *
 * Return: void
 */
-void rotl(stack_t **stack, unsigned int line_count __attribute__((unused)))
+void rotl(stack_t **stack, unsigned int line_count)
 {
 	stack_t *left;
 	stack_t *right;
 
+	(void) line_count;
 	if (!stack || !*stack || !(*stack)->next)
-	{
 		return;
-	}
+
 	left = right = *stack;
 
 	while (right->next) /* move the right pointer to the last node */
 		right = right->next;
 	right->next = left; /* a circle infinite linked list loop */
 	left->prev = right;
-	left->next = NULL;
-	*stack = left->next;
+	*stack = left->next; /* so we cut the link between the 0 and 1 element */
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
