8d2cf76ff6de436e6cf75c2dc39789c77d2e2d4f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-17 01:18:49 +0100

    Update other_function.c

--- other_function.c
+++ other_function.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include "monty.h"
 
+instruction_t instructions[] = {
+	{"push", push},
+	{NULL, NULL},
+};
 
 /**
  * opcode - function in charge of running builtins
@@ -14,6 +18,56 @@
  */
 void opcode(stack_t **stack __attribute__((unused)), char *str, unsigned int line_cnt __attribute__((unused)))
 {
-	printf("%s", str);
+	int i = 0;
+
+	if (!strcmp(str, "stack"))
+	{
+		global.data_struct = 1;
+		return;
+	}
+	if (!strcmp(str, "queue"))
+	{
+		global.data_struct = 0;
+		return;
+	}
+
+	while (instructions[i].opcode)
+	{
+		if (strcmp(instructions[i].opcode, str) == 0)
+		{
+			instructions[i].f(stack, line_cnt);
+			return; /* if we found a match, run the function */
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
+	status = EXIT_FAILURE;
+}
+void push(stack_t **stack, unsigned int line_cnt)
+{
+	char *n = argument;
+	int num = atoi(n);
+
+	if ((is_digit(n)) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+	stack_t *new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(new);
+		status = EXIT_FAILURE;
+		return;
+	}
+	new->n = num;
+
+	new->next = *stack;
+	new->prev = NULL;
+	if (*stack)
+		(*stack)->prev = new;
+	*stack = new;
 	return;
 }
