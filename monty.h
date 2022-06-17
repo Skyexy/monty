6101fb7c5ff9cb2a0ec1862e0b89a6cf2db5829f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-17 01:51:28 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -37,5 +37,6 @@ int is_digit(char *string);
 void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)));
 size_t print_stack(const stack_t *stack);
 void pint(stack_t **stack, unsigned int line_cnt);
+void pop(stack_t **stack, unsigned int line_cnt);
 
 #endif /* MONTY */
