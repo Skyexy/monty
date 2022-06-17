8aca6d01ed90e9425db408129b087f2ad1d0f504
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-18 00:06:14 +0100

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -123,3 +123,27 @@ stack_t *queue_node(stack_t **stack, const int n)
 
 	return (new);
 }
+
+/**
+ * free_stack - frees a dlistint_t linked list
+ * @stack: list head
+ *
+ * Return: void
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *current = stack;
+	stack_t *next;
+
+	if (stack)
+	{
+		next = stack->next;
+		while (current)
+		{
+			free(current);
+			current = next;
+			if (next)
+				next = next->next;
+		}
+	}
+}
