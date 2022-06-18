269ef3f09f246d14c45683005ca41cc15ead0c0b
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-18 20:36:19 +0100

    Create other_func.c

---
+++ other_func.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "monty.h"
+
+/**
+ * pop - computes the remainder of the division
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *point;
+
+	if(stack || *stack != NULL)
+	{
+		point = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(point);
+		return;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+}
+
+/**
+ * swap -  swaps data from top to previous
+ * @stack: stack given by main
+ * @line_cnt: ammount of lines
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *tmp = NULL;
+	int tmp_n = 0;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+	tmp = *stack;
+	tmp_n = tmp->n;
+
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
+
+}
+
+/**
+ * add - computes the remainder of the division
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_cnt)
+{
+	int result;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
+		status = EXIT_FAILURE;
+		return;
+	}
+
+	result = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, line_cnt);/*For top node*/
+	(*stack)->n = result;
+}
+
+/**
+ * nop - computes the remainder of the division
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void nop(stack_t **stack __attribute__((unused)), unsigned int line_cnt __attribute__((unused)))
+{
+	return;
+}
