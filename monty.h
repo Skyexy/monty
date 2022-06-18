f896f8393332202782fe542ab05789e959b7d646
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-18 20:55:44 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -30,23 +30,24 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-instruction_t instructions[] = {
-	{"push", push},
-	{"pall", pall},
-	{"pint", pint},
-	{"pop", pop},
-	{"swap", swap},
-	{"add", add},
-	{"nop", nop},
-	{"sub", sub},
-	{"div", _div},
-	{"mul", mul},
-	{"pchar", pchar},
-	{"pstr", pstr},
-	{"rotl", rotl},
-	{"rotr", rotr},
-	{NULL, NULL},
-};
+#define INSTRUCTIONS { \
+		{"push", push},\
+		{"pall", pall},\
+		{"pint", pint},\
+		{"pop", pop},\
+		{"swap", swap},\
+		{"nop", nop},\
+		{"div", _div},\
+		{"mul", mul},\
+		{"add", add},\
+		{"sub", sub},\
+		{"mod", mod},\
+		{"pchar", pchar},\
+		{"pstr", pstr},\
+		{"rotl", rotl},\
+		{"rotr", rotr},\
+		{NULL, NULL} \
+	}
 
 void opcode(stack_t **stack, char *str, unsigned int line_cnt);
 extern int status;
