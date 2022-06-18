8c279757a4f7d16f7249892361dcf4c2874cf78e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-18 20:53:27 +0100

    Update other_func.c

--- other_func.c
+++ other_func.c
@@ -87,8 +87,27 @@ void add(stack_t **stack, unsigned int line_cnt)
  *
  * Return: void
  */
-void nop(stack_t **stack , unsigned int line_cnt )
+void nop(stack_t **stack, unsigned int line_cnt)
 {
 	(void) stack;
 	(void) line_cnt;
+}
+/**
+ * pint - computes the remainder of the division
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_cnt)
+{
+	if (*stack != NULL)
+	{
+		printf("%d\n", (*stack)->n);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+		status = EXIT_FAILURE;
+	}
 }
